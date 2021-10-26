630e3d1e546a315530b9387d58eb8c5e22067899
Ankush Yadav <ankushyadav9302@gmail.com>
2021-10-27 04:48:29 +0530

    Add utility function to convert file sizes into human-readable format

--- frontend-react/src/shared/utils.js
+++ frontend-react/src/shared/utils.js
@@ -12,3 +12,11 @@ export const formatTime = (seconds) => {
 
     return str;
 }
+
+export const formatFileSize = (bytes, precision = 2) => {
+    if (bytes === 0) { return '0 Bytes' }
+
+    const k = 1024, sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+        i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(precision)) + ' ' + sizes[i];
+}
