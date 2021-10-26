83a99dafa2ba52b82ffdd79f483a46baaef5ae0e
Ankush Yadav <ankushyadav9302@gmail.com>
2021-10-27 02:49:21 +0530

    Rename from *.js to *.jsx

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigator from './Navigator';
+
+class App extends React.Component {
+    render () {
+        return (
+            <div className="App">
+               <Navigator />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ frontend-react/src/Navigator.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+    Route,
+    Switch,
+    BrowserRouter as Router
+} from "react-router-dom";
+import PlayAudio from "./components/PlayAudio";
+import UploadButton from "./components/UploadButton";
+
+export default class Navigator extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            fileName: null,
+            audioFile: null
+        };
+    }
+
+    setFileName = (fileName) => {
+        this.setState({
+            ...this.state,
+            fileName
+        });
+    }
+
+    setAudioFile = (audioFile) => {
+        this.setState({
+            ...this.state,
+            audioFile
+        });
+    }
+
+    render () {
+        return (
+            <div>
+                <Router>
+                    <Switch>
+                        <Route path="/upload-file">
+                            <UploadButton setFileName={this.setFileName} setAudioFile={this.setAudioFile} />
+                        </Route>
+                        <Route path="/play-audio">
+                            <PlayAudio fileName={this.state.fileName} audioFile={this.state.audioFile} />
+                        </Route>
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
+}
